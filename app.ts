a78ebf2e66242e760374c8a74a70cd79da6c3ac9
Juan Miguel <oliveirajuan243@gmail.com>
2023-09-15 18:30:33 -0300

    curso typescriptintrodução

---
+++ app.ts
@@ -0,0 +1,32 @@
+/*const soma =(a:number,b:number) =>{
+    console.log(a +b)
+}
+
+soma ()
+*/
+//declarando var
+/*
+let b: string = 'b';
+let n: number = 2;
+
+let m: string | number | boolean = 2
+m= 'Nath'
+m = false
+*/
+// para setar um variavel para tipo de objeto é
+/*
+const pessoa = {
+    nome :'juan',
+    idade: 21,
+    cidade:'Campo do Meio-mg'
+}
+*/
+//exemplo de como utilizar ts para objetos
+interface Pessoa {
+    nome: string,
+    idade :number,
+    cidade:string
+}
+const pessoa: Pessoa ={
+
+}
